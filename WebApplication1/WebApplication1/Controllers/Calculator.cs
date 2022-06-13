2eb14952bd05ed5bbc8295bfa51e0533c28fcce1
Pulkit Dhingra <pulkit.dhingra01@nagarro.com>
2022-06-13 15:35:09 +0530

    Initial commit

---
+++ WebApplication1/WebApplication1/Controllers/Calculator.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class Calculator : Controller
+    {
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Display(int val1, int val2, String Operator)
+        {
+            Calci values = new Calci();
+            int a = val1;
+            int b = val2;
+            String s = Operator;
+            if (s.ToLower()=="add")
+            {
+                values.answer = Add(a, b);
+            }
+            else if (s.ToLower() == "substract")
+            {
+                values.answer = Substract(a, b);
+            }
+
+            else if (s.ToLower() == "multiply")
+            {
+                values.answer = Multiply(a, b);
+            }
+            else if (s.ToLower() == "divide")
+            {
+                values.answer = Divide(a, b);
+            }
+            else {
+                values.answer = 0;
+            }
+            
+            return View(values);
+        }
+
+
+
+        public IActionResult Index()
+        {
+            Calci s = new Calci();      
+            return View();
+        }
+
+        public int Add(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+
+        public int Substract(int value1, int value2)
+        {
+            return value1 - value2;
+        }
+
+        public int Multiply(int value1, int value2)
+        {
+            return value1 * value2;
+        }
+
+        public int Divide(int value1, int value2)
+        {
+            return value1 / value2;
+        }
+
+        public ActionResult Display(Calci values)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Calci.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Calci
+    {
+        public int value1 { get; set; }
+        public int value2 { get; set; }
+
+        public String operatorr {get; set;}
+
+        public int answer { get; set; }
+    }
+}
